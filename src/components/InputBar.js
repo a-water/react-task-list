0ff4840912a47d7d3281460add4ab11c44562e18
Aaron <aaron_waterhouse@yahoo.com>
2019-03-15 16:22:46 -0700

    Added input bar with some styling

--- src/components/InputBar.js
+++ src/components/InputBar.js
@@ -1,10 +1,33 @@
 import React, { Component } from 'react';
 
 class InputBar extends Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      taskTitle: ''
+    }
+
+    this.handleOnSubmit = this.handleOnSubmit.bind(this);
+    this.handleOnInputChange = this.handleOnInputChange.bind(this);
+  }
+
+  handleOnSubmit() {
+
+  }
+
+  handleOnInputChange() {
+
+  }
+
   render() {
     return(
       <div>
-
+        <form className="input-form" onSubmit={ this.handleOnSubmit }>
+          <input type="text" placeholder="Enter task..." value={ this.state.taskTitle }></input>
+          <input type="submit" value="Add"></input>
+        </form>
       </div>
     )
   }
--- src/components/TodoContainer.js
+++ src/components/TodoContainer.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
+import InputBar from './InputBar';
 
 class TodoContainer extends Component {
   render() {
     return(
       <div className="container">
-
+        <InputBar />
       </div>
     )
   }
